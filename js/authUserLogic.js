58e31e10e91f0d8d1af92e298d2a1715fe8703f5
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-10-07 00:25:12 +0300

    add nav bar logic

---
+++ js/authUserLogic.js
@@ -0,0 +1,15 @@
+const loginBtn = document.getElementById("login-icon");
+const logoutBtn = document.getElementById("logout-icon");
+const profileBtn = document.getElementById("profile-icon");
+
+if (JSON.parse(localStorage.getItem("loggedInUser"))) {
+  loginBtn.style.display = "none";
+} else {
+  logoutBtn.style.display = "none";
+  profileBtn.style.display = "none";
+}
+
+logoutBtn.addEventListener("click", () => {
+  localStorage.removeItem("loggedInUser");
+  location.reload();
+});
--- js/navbar.js
+++ js/navbar.js
@@ -11,3 +11,6 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 });
+
+
+
